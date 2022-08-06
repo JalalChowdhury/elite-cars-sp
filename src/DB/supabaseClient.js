4f8e7f31c26af3a7021067563a1de5a7748b59db
Md. Jalal Uddin Chowdhury <79318370+JalalChowdhury@users.noreply.github.com>
2022-08-06 22:15:06 +0600

    done

--- src/DB/supabaseClient.js
+++ src/DB/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://ggofvsszogphgrbidbqk.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdnb2Z2c3N6b2dwaGdyYmlkYnFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTkyOTI4ODUsImV4cCI6MTk3NDg2ODg4NX0.I6PF_6duSioKhvGaflphptFl7bCJCRZJQPeScM1uE0I";
-
+const supabaseUrl = "";
+const supabaseAnonKey = "";
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
